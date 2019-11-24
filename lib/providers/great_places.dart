a17d5b02f5c3d7a2f5e343ac4900caef8ff9b737
HeKoDev <hekodev@gmail.com>
2019-11-24 15:28:01 -0500

    #6 : Managing Data & Images via the Provider Package

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
@@ -9,4 +11,17 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+  }
 }
